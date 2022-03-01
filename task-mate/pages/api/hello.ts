2f860dc8f6fbfec0cac0edea95b52848d7ed348e
Radoslav Velichkov <rado88x@gmail.com>
2022-03-01 10:55:37 +0200

    init config

---
+++ task-mate/pages/api/hello.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default (req: NextApiRequest, res:  NextApiResponse)  => {
+  res.status(200)
+  res.json({ name: "John Doe" });
+};
